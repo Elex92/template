21e69ea3a33a4c157e01710397b8d9129a2aedac
GitHub Action <action@github.com>
2023-01-13 02:55:21 +0000

    Template cleanup

--- src/main/kotlin/com/github/elex92/template/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/elex92/template/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.elex92.template.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.elex92.template.MyBundle
 
 class MyApplicationService {
 
